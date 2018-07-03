9bf956635332c5aff8e93d4f703af4174f3baa04
gusarov2k <motoface@yandex.ru>
2018-07-03 21:07:20 +0300

    add test product is not valid without a unique title

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -55,4 +55,17 @@ end
     end
   end
 
+  test "product is not valid without a unique title" do
+    # если у товара нет уникального названия, то он недопустим
+    product = Product.new(title: products(:ruby).title,
+    description:    "yyy",
+    price:          1,
+    image_url:      "fred.gif")
+
+    assert product.invalid?
+
+    assert_equal ["has already been taken"], product.errors[:title]
+                  # уже было использовано
+  end
+
 end
