457348c5281930d4da9d430798e10fc1060fd26e
gusarov2k <motoface@yandex.ru>
2018-07-06 14:24:57 +0300

    add communication

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ActiveRecord::Base
-	PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+	has_many :line_items, dependent: :destroy
+
+  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
 
 	validates :name, :address, :email, presence: true
 	validates :pay_type, inclusion: PAYMENT_TYPES
