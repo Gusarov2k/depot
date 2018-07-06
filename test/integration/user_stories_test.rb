ebf6ed83c8cbdcad671b122833ce4493c20bbae3
gusarov2k <motoface@yandex.ru>
2018-07-06 16:41:41 +0300

    change test

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -36,14 +36,10 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
     assert_equal 1, orders.size
     order = orders[0]
 
-    assert_equal
-    assert_equal
-    assert_equal
-    assert_equal
-    "Dave Thomas", order.name
-    "123 The Street", order.address
-    "dave@example.com", order.email
-    "Check", order.pay_type
+    assert_equal "Dave Thomas", order.name
+    assert_equal "123 The Street", order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check", order.pay_type
 
     assert_equal 1, order.line_items.size
     line_item = order.line_items[0]
