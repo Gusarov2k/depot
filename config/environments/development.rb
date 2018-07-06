b6b845e4f3398f5bd7ad6a8934091aa5d7108541
gusarov2k <motoface@yandex.ru>
2018-07-06 15:11:00 +0300

    off mail for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #off mail
+  Depot::Application.configure do
+    config.action_mailer.delivery_method = :test
+  end
+
 end
